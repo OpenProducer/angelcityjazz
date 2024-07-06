c2756477d51da1431402034702acffb80c46a3d6
Gus Austin <gusaus@gmail.com>
2024-07-05 17:17:05 -0700

    newspack theme update

--- wp-content/themes/newspack-theme/js/dist/post-subtitle.asset.php
+++ wp-content/themes/newspack-theme/js/dist/post-subtitle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => '26b0aa823a37f9f98061');
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => '54fd74ab0762e90a3868');
