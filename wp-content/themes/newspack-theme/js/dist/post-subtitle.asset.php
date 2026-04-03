9b9ebaf3f13a44028e0eb453420ab152e1299f7a
Gus Austin <gusaus@gmail.com>
2026-04-03 13:50:23 -0700

    Sync newspack-theme updates from master

--- wp-content/themes/newspack-theme/js/dist/post-subtitle.asset.php
+++ wp-content/themes/newspack-theme/js/dist/post-subtitle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '6cf7140d2f0aa4fa72a5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '43c470b8c9614fc75522');
